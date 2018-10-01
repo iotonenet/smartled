6c9a2dca2f0e614e80416f9723e208972e3370a0
songfei <fei.song@wz-inc.com>
2018-10-01 10:47:17 +0800

    add counter

---
+++ main/include/counter.h
@@ -0,0 +1,22 @@
+/*
+ * counter.h
+ *
+ *  Created on: Sep 30, 2018
+ *      Author: songfei
+ */
+
+#ifndef MAIN_INCLUDE_COUNTER_H_
+#define MAIN_INCLUDE_COUNTER_H_
+#include "stdint.h"
+/**
+ * increase 1
+ */
+void increase(void);
+
+/**
+ * get restart count;
+ */
+int32_t get_count();
+
+
+#endif /* MAIN_INCLUDE_COUNTER_H_ */
